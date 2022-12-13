a9eac0aaf5dd93b660c6ae090abfdc9cf06b9a01
sirkotal <lordkotal2002@gmail.com>
2022-12-13 12:24:21 +0000

    Created the Airport class

---
+++ src/airport.cpp
@@ -0,0 +1,34 @@
+#include "../headers/airport.h"
+
+Airport::Airport(string air_code, string name, string city, string country, double lat, double lon) {
+    this->air_code = air_code;
+    this->name = name;
+    this->city = city;
+    this->country = country;
+    this->lat = lat;
+    this->lon = lon;
+}
+
+string Airport::getAirCode() const{
+    return air_code;
+}
+
+string Airport::getName() const {
+    return name;
+}
+
+string Airport::getCity() const {
+    return city;
+}
+
+string Airport::getCountry() const {
+    return country;
+}
+
+double Airport::getLatitude() const {
+    return lat;
+}
+
+double Airport::getLongitude() const {
+    return lon;
+}
