4d5fc9eaaa948c5275e1f3ee6b9822073e16f2dc
sirkotal <lordkotal2002@gmail.com>
2022-12-29 15:46:12 +0000

    Added a reader function for airports.csv

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 #include "../headers/airport.h"
+#include "../headers/manager.h"
+
+#define airports "../air_data/airports.csv"
 
 using namespace std;
 
 int main() {
+    Manager man;
     Airport porto("OPO", "Francisco Sá Carneiro", "Porto", "Portugal", 41.0, 11.0);
     cout << "Hello World!" << endl;
     cout << porto.getCountry() << endl;
+    man.buildAirports(airports);
 }
