7c2016bb202c57d4f64bd3a89b6be2e3b5342f57
sirkotal <lordkotal2002@gmail.com>
2023-01-06 17:50:44 +0000

    Created files for the Menu class

---
+++ headers/menu.h
@@ -0,0 +1,15 @@
+#ifndef LEIC_AM_MENU_H
+#define LEIC_AM_MENU_H
+
+#include <bits/stdc++.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "manager.h"
+
+using namespace std;
+
+class Menu {};
+
+#endif
